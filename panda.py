d49f5ed1f31b55ca613d01a3657f3c93c7c590d1
FiollDoll <86582596+FiollDoll@users.noreply.github.com>
2022-04-26 12:35:04 +0500

    New Commit

--- panda.py
+++ panda.py
@@ -2,26 +2,27 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame ({ #Содаём таблицу
-     "Name": ["Artem", "Anton"],
+     "Name": ["Artem", "Anton"], #Значения
      "Balance": ["100", "0"],
      "Age": ["14", "5"]
 })
 
 print(df, "\n") #Полностью
 print(df[["Name", "Age"]], "\n") #Выбранное
-
+"""
 #Способы сохранения информации и сбора
 
 #Сохранение в Ексель
-writer = pd.ExcelWriter("balance.xlsx")
+writer = pd.ExcelWriter("name.xlsx")
 df.to_excel(writer)
 writer.save()
 
 #Чтение csv
-data = pd.read_csv("info.csv") #Через файл. Первая строка - значение
+data = pd.read_csv("name.csv") #Через файл. Первая строка - значение
 
 print(data)
 
 #Чтение Екселя
 data1 = pd.read_excel("info.excel") #Ексель
 print(data1)
+"""
